990b01b6b50bd94fe54f6567fb0ddd45a35d665c
ttalhacoskun <ttalhacoskun@gmail.com>
2023-12-02 02:15:31 +0300

    Initial Commit

---
+++ Git_Repostory_Deneme/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Git_Repostory_Deneme
+//
+//  Created by Talha Coşkun on 2.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Git_Repostory_Deneme/Git_Repostory_DenemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_Repostory_DenemeApp.swift
+//  Git_Repostory_Deneme
+//
+//  Created by Talha Coşkun on 2.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Git_Repostory_DenemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
